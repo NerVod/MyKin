1db3358d8eefd8a1a2c9473356e0bee388fc506f
NerVod <benjamin.jeannerot@gmail.com>
2022-05-08 16:19:01 +0200

    bouclage *ngFor pour affichage des wallposts

--- mykin/src/app/app.component.ts
+++ mykin/src/app/app.component.ts
@@ -8,20 +8,19 @@ import { WallPost } from './models/wall-post.model';
 })
 export class AppComponent implements OnInit {
 
-  myPost!: WallPost;
-  myOtherPost!: WallPost;
-  myLastPost!: WallPost;
+  wallPosts! : WallPost[]
 
     ngOnInit(): void {
-      this.myPost = { 
+      this.wallPosts = [
+      { 
         title : 'Tahiti',
         description : "Destination de rêve pour les vacances",
         createdDate : new Date(),
         likes :0,
         imageUrl : "https://www.capitainestudy.fr/wp-content/uploads/2019/10/tahiti-coline.jpg",
         location :'Tahiti'
-    }
-      this.myOtherPost = { 
+    },
+      { 
         title :'Paris Games Week',
         description :"Le rendez-vous annuel de la culture Gaming",
         createdDate : new Date(),
@@ -29,13 +28,13 @@ export class AppComponent implements OnInit {
         imageUrl : "https://www.usine-digitale.fr/mediatheque/0/3/0/000320030_homePageUne/paris-games-week-2015.jpg",
         location :'Paris'
       },
-      this.myLastPost = {
+        {
         title :'Fraisier',
         description :"Prise de poids d'un simple regard",
         createdDate :new Date(),
         likes :0,
         imageUrl :"https://gateaux-et-delices.com/wp-content/uploads/2015/04/G%C3%A2teau-aux-fraises-pour-accueillir-le-beau-temps1.jpg",
-        }
-    }
-
+        }  
+    ]; 
+  }
 }
