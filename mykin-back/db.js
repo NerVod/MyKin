a3121c63921b4d3599ab3a2b778eb6ca374936d9
NerVod <benjamin.jeannerot@gmail.com>
2022-05-18 19:21:38 +0200

    création db js pour création compte

---
+++ mykin-back/db.js
@@ -0,0 +1,29 @@
+const Mongoose = require('mongoose');
+require('dotenv').config();
+const url = process.env.DB;
+
+
+Mongoose.connect(url, { useNewUrlParser: true});
+
+const UserSchema = new Mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,  
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+
+}, { collection: 'users'})
+
+exports.User = Mongoose.model('User', UserSchema)
--- mykin-back/server.js
+++ mykin-back/server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
+const Database = require('./db');
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const salt = bcrypt.genSaltSync(saltRounds);
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
 const app = express();
 
 const config = {
@@ -14,6 +21,42 @@ app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({ messagejson: "route principale délivrée par le backend" });
+});
+
+///////////////////// Création compte utilistateur/////////////////////
+app.post("/register", (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    res.json({
+      "Erreur création de compte": "vérifier email ou de mot de passe",
+    });
+    return
+  }
+
+  Database.User.findOne({email: req.body.email }).then((user) => {
+      if(user) {
+          res.json({"Compte existant": "Cet email est associé à un compte"})
+      } else {
+          Database.User.create({
+              username: req.body.username,
+              email: req.body.email,
+              password: bcrypt.hashSync(req.body.password, salt)
+          })
+          .then((user) => {
+              console.log("compte créé en base de données")
+
+              const token = jwt.sign(
+                  { id: user._id, email: user.email, username: user.username }, process.env.JWTPRIVATEKEY
+              );
+              console.log("token créé à la création du compte :", token);
+              res.setHeader("Authorization", "Bearer " + token);
+              res.json({"compte créé": "Compte créé avec succès en base de données"})
+          })
+          .catch((err) => {
+              console.error(err);
+              res.json({"Erreur création": "compte non activé"})
+          });
+      };
+  });
 });
 
 const httpServer = app.listen(config.port, () => {
