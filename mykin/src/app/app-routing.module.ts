bd9a262b196fa30f2c06be40fea3fa70a1a382b6
NerVod <benjamin.jeannerot@gmail.com>
2022-05-18 19:35:25 +0200

    routes log register + correct position footer

--- mykin/src/app/app-routing.module.ts
+++ mykin/src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LogInComponent } from './log-in/log-in.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {path: "", pathMatch: "full", redirectTo: "home"},
-  {path:"home", component: HomeComponent },
+  {path:"home", component: LogInComponent },
+  {path: "register", component: RegisterComponent},
+  {path: "login", component: LogInComponent}
   
 ];
 
