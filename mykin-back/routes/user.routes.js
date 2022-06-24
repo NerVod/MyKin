1510c58c561eb7276e5ef7e671b93f7066a019b9
NerVod <benjamin.jeannerot@gmail.com>
2022-06-24 16:29:30 +0200

    clean routes

--- mykin-back/routes/user.routes.js
+++ mykin-back/routes/user.routes.js
@@ -3,15 +3,14 @@ const router = express.Router();
 const userHandler = require("./userHandler");
 const auth = require("../middleware/auth");
 const contactHandler= require("./contactHandler")
-const uploadHandler = require('./uploadHandler')
-const parseMp = require('express-parse-multipart')
+
 
 
 
 
 router.post("/register", userHandler.registerNewUser);
 router.post("/login", userHandler.loginUser);
-router.post("/upload", parseMp, uploadHandler.onFileUpload)
+
 
 // router.get("/photoprofile/:id", userHandler.photoprofile )
 router.get("/logged", auth, userHandler.dataProtegee);
@@ -24,74 +23,3 @@ router.get("/invitecontact/:id/:param", auth,  contactHandler.invitationContact 
 module.exports = router;
 
 
-
-
-// let User = require("../model/User");
-
-// // add a user
-// userRoute.route("/add-user").post((req, res, next) => {
-//   User.create(req.body, (error, data) => {
-//     if (error) {
-//       console.log('erreur route adduser')
-//       return next(error);
-//     } else {
-//       res.json(data);
-//     }
-//   });
-// });
-
-// // get all users
-// userRoute.route("/").get((req, res) => {
-//   User.find((error, data) => {
-//     if (error) {
-//       return next(error);
-//     } else {
-//       res.json(data);
-//     }
-//   });
-// });
-
-// // Get a user
-// userRoute.route("/show-user/:id").get((req, res) => {
-//   User.dindById(req.params.id, (error, data) => {
-//     if (error) {
-//       return next(error);
-//     } else {
-//       res.json(data);
-//     }
-//   });
-// });
-
-// // update a user
-// userRoute.route("/update-user/:id").put((req, res, next) => {
-//   User.findByIdAndUpdate(
-//     req.params.id,
-//     {
-//       $set: req.body,
-//     },
-//     (error, data) => {
-//       if (error) {
-//         console.log(error);
-//         return next(error);
-//       } else {
-//         res.json(data);
-//         console.log("User mis à jour avec succès");
-//       }
-//     }
-//   );
-// });
-
-// // delete a User
-// userRoute.route("/delete-user/:id").delete((req, res, next) => {
-//   User.findByIdAndRemove(req.params.id, (error, data) => {
-//     if (error) {
-//       return next(error);
-//     } else {
-//       res.status(200).json({
-//         message: data,
-//       });
-//     }
-//   });
-// });
-
-// module.exports = userRoute;
