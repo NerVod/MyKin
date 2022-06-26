4236e2e8dc09f80f8abe416b18fbc94d88d9b5a1
NerVod <benjamin.jeannerot@gmail.com>
2022-06-26 10:18:37 +0200

    route back pour accepter nouvel ami

--- mykin-back/routes/user.routes.js
+++ mykin-back/routes/user.routes.js
@@ -21,6 +21,7 @@ router.get("/invitecontact/:id/:param", auth,  contactHandler.invitationContact 
 router.get("/updatedemandeenvoyee/:id/:param", auth, contactHandler.updatedemandeenvoyee)
 router.get("/isinvited/:id/:param", auth, contactHandler.isinvited)
 router.get("/getinvitattente/:id",  contactHandler.getInvitAttente)
+router.get("/accepterami/:id/:param", auth,  contactHandler.accepterAmi)
 
 
 module.exports = router;
